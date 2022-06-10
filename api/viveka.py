f2640bc41cd8f567ee0097c52fdc575d3905e356
jarusified <jarus3001@gmail.com>
2022-06-09 19:42:58 -0700

    Add basic structure

---
+++ api/viveka.py
@@ -0,0 +1,42 @@
+#  Because pybind11 cannot generate default parameters well, this code is to set
+#  them
+
+class Viveka:
+    """An extension version of the change point detection using adaptive
+    forgetting factor with PCA for multivariate streaming data.
+    Parameters
+    ----------
+    
+    Attributes
+    ----------
+    Examples
+    --------
+    >>> import viveka
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def start(self):
+        """Start profiling
+        Parameters
+        ----------
+       
+        Returns
+        -------
+        self: object
+            Returns the instance itself.
+        """
+        return super().start()
+
+    def stop(self):
+        """Stop profiling
+        Parameters
+        ----------
+        None
+        Returns
+        -------
+        self: object
+            Returns the instance itself.
+        """
+        return super().stop()
